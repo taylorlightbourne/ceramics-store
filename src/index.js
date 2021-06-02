ed89543389001c3c965026f61dfe82de797e2450
Taylor Lightbourne <79942688+taylorlightbourne@users.noreply.github.com>
2021-06-02 12:05:46 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./reducers/rootReducer";
+import logger from "redux-logger";
+
+const store = createStore(rootReducer, applyMiddleware(logger));
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
